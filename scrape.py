2b7ba9fafa052adf5a1b50fcc6d4a9e4c6dde161
Christopher Story <cstory@liventus.com>
2023-08-04 09:21:40 -0500

    Updates to handle multiple classes and better formatting of text output

--- scrape.py
+++ scrape.py
@@ -3,42 +3,37 @@ import os
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 import sys
+import re
 
-def format_text(soup):
-    # Add line breaks between different elements for minimal formatting
-    for tag in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'br']):
-        tag.append('\n')
-    return soup.get_text()
-
-def format_text(soup):
-    # Add line breaks between different elements for minimal formatting
-    for tag in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'br']):
-        tag.append('\n')
-    text_content = soup.get_text()
-    # Remove any non-readable or special characters
-    text_content = re.sub(r'[^\w\s.,?!;:\-\'"]', '', text_content)
+def format_text(soup, classes):
+    """Extract and format text from elements with the specified classes."""
+    text_content = []
+    for class_name in classes:
+        for element in soup.find_all(class_=class_name):
+            text_content.append(element.get_text())
+    text_content = '\n'.join(text_content)
+    text_content = re.sub(r'[^\w\s.,?!;:\-\'"]+', '', text_content)
     return text_content
 
-def download_page_as_text(driver, url, filename):
+def download_page_as_text(driver, url, filename, classes):
+    """Download a web page as a plain text file."""
     try:
         driver.get(url)
-        # Get the page source and extract formatted text using BeautifulSoup
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        text_content = format_text(soup)
+        text_content = format_text(soup, classes)
         with open(filename, 'w', encoding='utf-8') as file:
             file.write(text_content)
         print(f'Saved {filename} as plain text')
     except Exception as e:
         print(f'Error saving {url} as text: {e}')
 
-def scrape_and_save_subpages(url, output_folder='text_output'):
-    # Configure the Selenium web driver for Chrome
+def scrape_and_save_subpages(url, classes, output_folder='text_output'):
+    """Scrape and save subpages from the given URL as text files."""
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=options)
 
-    # Ensure the output folder exists
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -52,20 +47,15 @@ def scrape_and_save_subpages(url, output_folder='text_output'):
 
         visited_urls.add(current_url)
 
-        driver.get(current_url)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        # Save the current page as plain text
         filename_base = urlparse(current_url).path.replace('/', '_').strip('_')
-        # Remove any trailing underscores
         filename_base = filename_base.rstrip('_')
         text_file = os.path.join(output_folder, filename_base + '.txt')
-        download_page_as_text(driver, current_url, text_file)
 
-        # Find all links and add them to the list if they are on the same domain
-        for link in soup.find_all('a'):
+        download_page_as_text(driver, current_url, text_file, classes)
+
+        for link in BeautifulSoup(driver.page_source, 'html.parser').find_all('a'):
             href = link.get('href')
-            if href and href.startswith('/'): # Ensure it's a relative link on the same domain
+            if href and href.startswith('/'):
                 sub_url = urljoin(current_url, href)
                 if sub_url not in visited_urls:
                     urls_to_visit.append(sub_url)
@@ -73,13 +63,12 @@ def scrape_and_save_subpages(url, output_folder='text_output'):
     driver.quit()
 
 if __name__ == "__main__":
-    # Check if the URL is provided
     if len(sys.argv) < 2:
-        print("Please provide the URL as an argument.")
+        print("Please provide the URL as the first argument.")
         sys.exit(1)
 
-    # Get the URL from the command-line argument
     url = sys.argv[1]
+    # Get the classes from the command-line argument, or use the default
+    classes = sys.argv[2].split(',') if len(sys.argv) > 2 else ['theme-doc-markdown']
 
-    # Start the scraping
-    scrape_and_save_subpages(url)
+    scrape_and_save_subpages(url, classes)
